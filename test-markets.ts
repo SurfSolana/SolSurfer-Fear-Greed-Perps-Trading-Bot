69e96106556b708d2ad4a21322fe3ca80a084e00
Alex Newman <thedotmack@gmail.com>
2025-09-15 16:53:07 -0400

    refactor: Replace getMarketIndex with getPerpMarket for market index retrieval and remove deprecated market constants

---
+++ test-markets.ts
@@ -0,0 +1,6 @@
+import { MainnetPerpMarkets, DevnetPerpMarkets } from '@drift-labs/sdk';
+
+console.log('MainnetPerpMarkets:', MainnetPerpMarkets);
+console.log('\nETH market:', MainnetPerpMarkets[2]);
+console.log('\nBTC market:', MainnetPerpMarkets[1]);
+console.log('\nSOL market:', MainnetPerpMarkets[0]);
